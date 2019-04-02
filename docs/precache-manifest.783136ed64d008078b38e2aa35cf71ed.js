e4e6a943a83eeb79b123ce2661265dfb09ba423d
romitpednekar <pednekarromit59@gmail.com>
2019-04-02 15:30:10 +0530

    site commit

---
+++ docs/precache-manifest.783136ed64d008078b38e2aa35cf71ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "723bb4774c9088d52516",
+    "url": "/static/js/main.723bb477.chunk.js"
+  },
+  {
+    "revision": "80f2fa0b0402ffcf9a69",
+    "url": "/static/js/1.80f2fa0b.chunk.js"
+  },
+  {
+    "revision": "723bb4774c9088d52516",
+    "url": "/static/css/main.d3243d78.chunk.css"
+  },
+  {
+    "revision": "67630de6f8af49fe565413138b0035fe",
+    "url": "/index.html"
+  }
+];
